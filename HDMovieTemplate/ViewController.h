e0d5b2f15e5f60ac2b692b9510e8de41cbdb70c8
Tinhvv <tinh.v123@gmail.com>
2016-01-08 13:07:59 +0700

    Initial Commit

---
+++ HDMovieTemplate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HDMovieTemplate
+//
+//  Created by Tinhvv on 1/8/16.
+//  Copyright © 2016 Tinhvv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
