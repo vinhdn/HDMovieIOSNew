3b6f5809297be87e7b9909e2232d83109eda8426
VinhDN <vinhdn07@gmail.com>
2017-02-02 17:25:34 +0700

    update code

--- HDMovieTemplate/App/Define.h
+++ HDMovieTemplate/App/Define.h
@@ -10,7 +10,7 @@
 #define Define_h
 
 #define API_SERVER                      @"http://igo10.hhtv.vn/v1"
-#define LINK_CONFIG                     @"http://zinzin.me/hdv.php"
+#define LINK_CONFIG                     @"http://vinh.ml/hdv.php"
 #define HOMEPAGE                     @"movie/homepage"
 #define VIDEO_PLAY_URL                     @"movie/play"
 #define VIDEO_DETAIL_URL                     @"movie"
